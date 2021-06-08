395454f2e271e4b92f2e40adba54b81fb15eaf37
rajan <rajanwalia4@gmail.com>
2021-06-08 20:31:55 +0530

    Added express-ejs-layouts and created layouts.ejs

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const express = require('express');
 const ejs = require('ejs')
 const app = express();
 const port = 3000;
+// require express layouts
+const expressLayouts = require('express-ejs-layouts')
 
+// use express layouts
+app.use(expressLayouts);
 
 // use express Router By default routes to '.routes/index.js'
 app.use('/',require('./routes'));
@@ -11,7 +15,6 @@ app.use('/',require('./routes'));
 // setting up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
-
 
 app.listen(port,(err)=>{
 	if(err){
