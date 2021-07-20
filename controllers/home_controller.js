c503fe0bc7cbb0a86b6fc9efcfdb0a8c1f504538
rajan <rajanwalia4@gmail.com>
2021-07-20 15:52:15 +0530

    Convert the home_controller into async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,8 +1,38 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = (req,res)=>{
+module.exports.home = async (req,res)=>{
+	try{
+		let posts = await Post.find({}) // wait for this to complete
+		.populate('user')
+		.populate({  // Also populate comments on each post along with the user who commented
+			path:'comments',
+			populate:{
+				path:'user' // Nesting population comments and user
+			}
+		});
+		
+		let users = await User.find({}); // wait for this task to complete
+		
+		return res.render('home',{
+			title: "Home Page",
+			posts : posts,
+			all_users:users
+		});
+		
+	}catch(err){
+		console.log("Error : ",err);
+		return ;
+	}
 	
+}
+
+module.exports.about = (req,res)=>{
+	return res.end('<h2> About</h2>')
+}
+
+
+//module.exports.home = (req,res)=>{
 	// Getting cookies from browser
 	//console.log(req.cookies);
 	
@@ -22,32 +52,28 @@ module.exports.home = (req,res)=>{
 	// 	})
 	// })
 	// Populate the user of each post 
-	Post.find({})
-	.populate('user')
-	.populate({  // Also populate comments on each post along with the user who commented
-		path:'comments',
-		populate:{
-			path:'user' // Nesting population comments and user
-		}
-	})
-	.sort({ createdAt: -1 })	// sort the posts creation timestamp
-	.exec((err,posts)=>{
-		User.find({},function(err,users){
-			if(err){
-				console.log("Error in finding the posts");
-				return ;
-			}
-			return res.render('home',{
-				title: "Home Page",
-				posts : posts,
-				all_users:users
-			});
-		});
-		
-	})
-	
-}
+	// Post.find({})
+	// .populate('user')
+	// .populate({  // Also populate comments on each post along with the user who commented
+	// 	path:'comments',
+	// 	populate:{
+	// 		path:'user' // Nesting population comments and user
+	// 	}
+	// })
+	// .sort({ createdAt: -1 })	// sort the posts creation timestamp
+	// .exec((err,posts)=>{
+	// 	User.find({},function(err,users){
+	// 		if(err){
+	// 			console.log("Error in finding the posts");
+	// 			return ;
+	// 		}
+	// 		return res.render('home',{
+	// 			title: "Home Page",
+	// 			posts : posts,
+	// 			all_users:users
+	// 		});
+	// 	});
 
-module.exports.about = (req,res)=>{
-	return res.end('<h2> About</h2>')
-}
+	// })
+	
+//}
