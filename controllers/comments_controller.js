e32bc0adcee60be3a9880ba01df3bcb259f405b8
rajan <rajanwalia4@gmail.com>
2021-07-25 17:39:00 +0530

    Added flash messages at signIn,signOut,create post ,delete post, add comment ,delete comment using noty

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -17,38 +17,14 @@ module.exports.create = async (req,res)=>{
 			// push the comment in the post 
 			post.comments.push(comment);
 			post.save();
-			
-			res.redirect('/');
+			req.flash("success","Comment added");
+			return res.redirect('/');
 		}
 	}catch(err){
-		console.log("Error : ",err);
-		return ;
+		req.flash("error",err);
+		return res.redirect('/') ;
 	}	
 }
-
-// module.exports.create = (req,res)=>{
-	
-// 	Post.findById(req.body.post,(err,post)=>{
-// 		//console.log(post);
-// 		if(post){
-			
-// 			//comment of the post
-// 			Comment.create({
-// 				content:req.body.content,  // content of comment
-// 				user: req.user._id, // user who created comment
-// 				post: req.body.post // post id 
-// 			},(err,comment)=>{
-// 				// push the comment in the post 
-// 				post.comments.push(comment);
-// 				post.save();
-				
-// 				res.redirect('/');
-// 			})
-// 		}
-		
-// 	})
-
-// }
 
 // using asyc await
 module.exports.destroy = async (req,res)=>{
@@ -66,40 +42,16 @@ module.exports.destroy = async (req,res)=>{
 			
 			// following is to delete the comment id from the post collection's comments Array 
 			Post.findByIdAndUpdate(postId,{ $pull:{comments:req.params.id} }, (err,post)=>{
+				req.flash("error","Comment deleted");
 				return res.redirect("back");
 			});
 		}else{
+			req.flash("error","You can't delete this comment");
 			return res.redirect("back");
 		}
 	}catch(err){
-		console.log("Error : ",err);
-		return ;
+		req.flash("error",err);
+		return res.redirect("back");
 	}
 
 }
-
-
-// // To delete the comment from the Comment and its id from the Post collections
-// module.exports.destroy = (req,res)=>{
-	
-	
-// 	// find the comment id in the Comment collection
-// 	Comment.findById(req.params.id,(err,comment)=>{
-				
-// 		// if authenticated user is the commented user
-// 		if(comment.user == req.user.id){
-// 			//  save the comment's postId into the following variable to delete this comment id from 
-// 			// corresponding post comment array
-// 			let postId = comment.post;
-// 			comment.remove();
-			
-// 			// following is to delete the comment id from the post collection's comments Array 
-// 			Post.findByIdAndUpdate(postId,{ $pull:{comments:req.params.id} }, (err,post)=>{
-// 				return res.redirect("back");
-// 			});
-// 		}else{
-// 			return res.redirect("back");
-// 		}
-		
-// 	});
-// }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -12,31 +12,16 @@ module.exports.create = async (req,res)=>{
 		let post = await Post.create({
 			content : req.body.content,
 			user : req.user._id
-		})
+		});
+		req.flash('success',"Post created successfully");
 		return res.redirect("back");
 	}
 	catch(err){
-		console.log("Error : ",err);
-		return ;
+		req.flash('error',err);
+		return res.redirect("back");
 	}
 	
 }
-
-// module.exports.create = (req,res)=>{
-// 	const content = req.body.content;
-// 	const user = req.user._id;
-// 	Post.create({
-// 		content : req.body.content,
-// 		user: req.user._id	
-// 	},(err,post)=>{
-// 		if(err){
-// 			console.log("Error in creating post");
-// 			return ;
-// 		}
-// 		console.log("post successfully created");
-// 		return res.redirect('/');
-// 	});
-// }
 	
 module.exports.destroy = async (req,res)=>{
 	let post = await Post.findById(req.params.id);
@@ -46,35 +31,17 @@ module.exports.destroy = async (req,res)=>{
 			
 			// Also delete its comments
 			await Comment.deleteMany({post:req.params.id});
+			req.flash('success',"Post and its associated comment are deleted!");
 			return res.redirect("back");
 		}else{
+			res.flash('error',"You can't delete this post");
 			return res.redirect("back");
 		}
 	}catch(err){
-		console.log("Error : ",err);
-		return ;
+		req.flash('error',err);
+		return res.redirect("back");
 	}
 	
-	
-}
-
-
-// To delete a Post and its corresponding comments
-module.exports.destroy = (req,res)=>{
-	Post.findById(req.params.id,(err,post)=>{
-		// .id means converting the Object id into String
-		if(post.user == req.user.id){ // user is the creator of the post
-			post.remove();
-			
-			// Also delete its comments
-			Comment.deleteMany({post:req.params.id},(err)=>{
-				return res.redirect("back");
-			});
-		}else{
-			// user is not the creator of the post
-			return res.redirect("back");
-		}
-	});
 	
 }
 	
